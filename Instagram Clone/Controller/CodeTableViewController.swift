853c20f59d2018b890a88eeb38f5afc11fc40105
dongdong3344 <dongdong3344@163.com>
2017-09-05 23:49:26 +0800

    完成 code tableView

--- Instagram Clone/Controller/CodeTableViewController.swift
+++ Instagram Clone/Controller/CodeTableViewController.swift
@@ -12,6 +12,7 @@ class CodeTableViewController: UITableViewController {
     
     var mobileCodes = [MobileCode]()
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,7 +29,11 @@ class CodeTableViewController: UITableViewController {
     
     // MARK: - Table view data source
 
-   
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
        
         return mobileCodes.count
@@ -40,7 +45,14 @@ class CodeTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
         
         cell.textLabel?.text = mobileCodes[indexPath.row].name
-
+        
+       
+        let dialCodeLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 80, height: 24))
+        dialCodeLabel.textAlignment = .right
+        dialCodeLabel.textColor = UIColor.lightGray
+        dialCodeLabel.font = UIFont.systemFont(ofSize: 15)
+        dialCodeLabel.text = mobileCodes[indexPath.row].dialCode
+        cell.accessoryView = dialCodeLabel
 
         return cell
     }
