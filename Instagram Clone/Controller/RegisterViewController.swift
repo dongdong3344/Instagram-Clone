90e417b80e9280a0c4a556f2bf02644b7037ee93
dongdong3344 <dongdong3344@163.com>
2017-09-06 13:59:52 +0800

    电话，邮箱按钮切换方法实现

--- Instagram Clone/Controller/RegisterViewController.swift
+++ Instagram Clone/Controller/RegisterViewController.swift
@@ -9,36 +9,47 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+ 
+    @IBOutlet weak var leftConstraint: NSLayoutConstraint!
     @IBOutlet weak var codeButton: UIButton!
+    @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var phoneButton: UIButton!
+    var lastSelectedButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+       
+        phoneButton.isSelected = true
+        
+        lastSelectedButton = phoneButton
+       
     }
 
+    @IBAction func buttonClick(_ sender: UIButton) {
+        
+        sender.isSelected = !sender.isSelected
+        sender.setTitleColor(UIColor.black, for: .normal)
+        lastSelectedButton.isSelected = false
+        lastSelectedButton.setTitleColor(UIColor.lightGray, for: .normal)
+        lastSelectedButton = sender
+        //改变约束
+        leftConstraint.constant = sender.frame.origin.x + 20
+        //动画效果
+        UIView.animate(withDuration: 0.25) {
+            self.view.layoutIfNeeded()
+        }
+     
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        
+    
         if let title = UserDefaults.standard.object(forKey: "CodeButtonTitle") as? String {
             codeButton.setTitle(title, for: .normal)
         }
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+   
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
