c23b816d5d42f48f5574afed27b706337b87cda4
dongdong3344 <dongdong3344@163.com>
2017-09-10 08:25:54 +0800

    使用extension

--- Instagram Clone/Controller/ProfileViewController.swift
+++ Instagram Clone/Controller/ProfileViewController.swift
@@ -20,27 +20,25 @@ class ProfileViewController: UIViewController {
     
     func addRightBarButtonItems(){
         
-        let saveHome = createButtonItemWithImage(#imageLiteral(resourceName: "bar-button-savehome"), action:#selector(ProfileViewController.saveHome))
-        let archive = createButtonItemWithImage(#imageLiteral(resourceName: "bar-button-archive"), action: #selector(ProfileViewController.archive))
-        let favorites = createButtonItemWithImage(#imageLiteral(resourceName: "bar-button-favorites"), action: #selector(ProfileViewController.favorites))
-        let discover = createButtonItemWithImage(#imageLiteral(resourceName: "bar-button-discover-people-2"), action: #selector(ProfileViewController.discover))
-        let more = createButtonItemWithImage(#imageLiteral(resourceName: "bar-button-more"), action: #selector(ProfileViewController.more))
         
-        navigationItem.rightBarButtonItems = [more,discover,favorites,archive,saveHome]
+        let saveHome = UIBarButtonItem.itemWithImage(#imageLiteral(resourceName: "bar-button-savehome"), action:#selector(ProfileViewController.saveHome))
+        
+        let archive = UIBarButtonItem.itemWithImage(#imageLiteral(resourceName: "bar-button-archive"), action: #selector(ProfileViewController.archive))
+        
+        let favorites = UIBarButtonItem.itemWithImage(#imageLiteral(resourceName: "bar-button-favorites"), action: #selector(ProfileViewController.favorites))
+        
+        let discover = UIBarButtonItem.itemWithImage(#imageLiteral(resourceName: "bar-button-discover-people-2"), action: #selector(ProfileViewController.discover))
+        
+        let more = UIBarButtonItem.itemWithImage(#imageLiteral(resourceName: "bar-button-more"), action: #selector(ProfileViewController.more))
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        spacer.width = -15
+        navigationItem.rightBarButtonItems = [spacer,more,discover,favorites,archive,saveHome]
         
         
     }
 
-    func createButtonItemWithImage(_ image:UIImage,action:Selector) ->UIBarButtonItem{
-        
-        let itemButton = UIButton(type: .custom)
-        itemButton.setImage(image, for: [])
-        itemButton.tintColor = UIColor.black
-        itemButton.addTarget(self, action: action, for: .touchUpInside)
-        itemButton.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
-        return UIBarButtonItem(customView: itemButton)
-        
-    }
+
     
    @objc func more(){
         
---
+++ Instagram Clone/Helper/Extension.swift
@@ -0,0 +1,24 @@
+//
+//  Extension.swift
+//  Instagram Clone
+//
+//  Created by lindongdong on 2017/9/10.
+//  Copyright © 2017年 Ringo Lin. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIBarButtonItem {
+    i
+   static func itemWithImage(_ image:UIImage,action:Selector) -> UIBarButtonItem{
+        
+        let itemButton = UIButton(type: .custom)
+        itemButton.setImage(image, for: [])
+        itemButton.tintColor = UIColor.black
+        itemButton.addTarget(self, action: action, for: .touchUpInside)
+        itemButton.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        return UIBarButtonItem(customView: itemButton)
+        
+    }
+}
